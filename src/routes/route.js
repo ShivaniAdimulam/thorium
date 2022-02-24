3313f85cd655781ad5ffdabf4feb5035512e4892
ShivaniAdimulam <adimulamshivani@gmail.com>
2022-02-24 23:16:19 +0530

    24-february-2022 Assignment Completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,48 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+
+router.post("/query", function(req,res){
+    let arr=[];
+    let persons= [
+        {
+        'name': 'PK',
+        'age': 10,
+        'votingStatus': false
+       },
+       {
+        'name': 'SK',
+        'age': 20,
+        'votingStatus': false
+       },
+       {
+        'name': 'AA',
+        'age': 70,
+        'votingStatus': false
+       },
+       {
+        'name': 'SC',
+        'age': 5,
+        'votingStatus': false
+       },
+       {
+        'name': 'HO',
+        'age': 40,
+        'votingStatus': false
+       }
+       ]
+       let votingAge=req.query.votingAge
+
+
+       for(let i=0;i<persons.length;i++){
+           if(persons[i].age>votingAge){
+               persons[i].votingStatus=true
+               arr.push(persons[i])
+           }
+           
+       }
+       return res.send({result:arr,status:true})
+});
+
+
 module.exports = router;

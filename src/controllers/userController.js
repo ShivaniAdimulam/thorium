0c9b30e1a132531879cdb658e09eba857866a3f9
ShivaniAdimulam <adimulamshivani@gmail.com>
2022-03-01 00:47:31 +0530

    assignment for 28-february-2022 completed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,7 @@
 const UserModel= require("../models/userModel")
 
+
+
 const createUser= async function (req, res) {
     let data= req.body
     let savedData= await UserModel.create(data)
@@ -11,5 +13,7 @@ const getUsersData= async function (req, res) {
     res.send({msg: allUsers})
 }
 
+
 module.exports.createUser= createUser
 module.exports.getUsersData= getUsersData
+
